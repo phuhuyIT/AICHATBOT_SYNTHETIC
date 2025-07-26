0748e1a2b38c6cd41f70661c577920e3df99cca1
phuhuykoxl <phuhuy02003@gmail.com>
2025-07-26 21:17:56 +0700

    Add soft delete functionality and enhanced query methods in `GenericRepository` via FindAsync

--- WebApplication1/Repository/GenericRepository.cs
+++ WebApplication1/Repository/GenericRepository.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using WebApplication1.Data;
+using WebApplication1.Models;
 using WebApplication1.Repository.Interface;
 
 namespace WebApplication1.Repository
@@ -32,19 +35,107 @@ namespace WebApplication1.Repository
             return true;
         }
 
+        public async Task<T?> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
         public virtual async Task<IEnumerable<T>> GetAllAsync()
         {
-            return await _dbSet.AsNoTracking().ToListAsync();
+            var query = _dbSet.AsNoTracking();
+            
+            // For other entities, filter by IsActive
+            var isActiveProperty = typeof(T).GetProperty("IsActive");
+            if (isActiveProperty != null)
+            {
+                return await query.Where(e => (bool)isActiveProperty.GetValue(e)).ToListAsync();
+            }
+
+            // If no IsActive property, return all
+            return await query.ToListAsync();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllActiveAsync()
+        {
+            var query = _dbSet.AsNoTracking();
+
+            // For entities with IsActive property, filter by IsActive
+            var isActiveProperty = typeof(T).GetProperty("IsActive");
+            if (isActiveProperty != null)
+            {
+                return await query.Where(e => (bool)isActiveProperty.GetValue(e)).ToListAsync();
+            }
+
+            // If no IsActive property, return all
+            return await query.ToListAsync();
         }
 
         public virtual async Task<T?> GetByIdAsync(Guid id)
         {
-            return await _dbSet.FindAsync(id);
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+                return null;
+
+            // For entities with IsActive property, check if active
+            var isActiveProperty = typeof(T).GetProperty("IsActive");
+            if (isActiveProperty != null)
+            {
+                var isActive = (bool)isActiveProperty.GetValue(entity);
+                return isActive ? entity : null;
+            }
+
+            return entity;
         }
 
         public virtual async Task UpdateAsync(T entity)
         {
             _dbSet.Update(entity);
+        }
+
+        // Soft delete methods
+        public virtual async Task<bool> SoftDeleteAsync(Guid id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+                return false;
+            
+            // For entities with IsActive property, set IsActive = false
+            var isActiveProperty = typeof(T).GetProperty("IsActive");
+            if (isActiveProperty != null && isActiveProperty.CanWrite)
+            {
+                isActiveProperty.SetValue(entity, false);
+                return true;
+            }
+
+            return false; // Entity doesn't support soft delete
+        }
+
+        public virtual async Task<bool> RestoreAsync(Guid id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+                return false;
+
+            // For entities with IsActive property, set IsActive = true
+            var isActiveProperty = typeof(T).GetProperty("IsActive");
+            if (isActiveProperty != null && isActiveProperty.CanWrite)
+            {
+                isActiveProperty.SetValue(entity, true);
+                return true;
+            }
+
+            return false; // Entity doesn't support soft delete
+        }
+
+        // Methods to get all entities including soft-deleted ones (for admin purposes)
+        public virtual async Task<IEnumerable<T>> GetAllIncludingDeletedAsync()
+        {
+            return await _dbSet.AsNoTracking().ToListAsync();
+        }
+
+        public virtual async Task<T?> GetByIdIncludingDeletedAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
         }
     }
 }
--- WebApplication1/Repository/Interface/IGenericRepository.cs
+++ WebApplication1/Repository/Interface/IGenericRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApplication1.Repository.Interface
+﻿using System.Linq.Expressions;
+
+namespace WebApplication1.Repository.Interface
 {
     public interface IGenericRepository<T> where T : class
     {
@@ -8,5 +10,15 @@
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
         Task<bool> DeleteAsync(Guid id);
+        Task<T?> FindAsync(Expression<Func<T, bool>> match);
+        
+        // Soft delete methods
+        Task<bool> SoftDeleteAsync(Guid id);
+        Task<bool> RestoreAsync(Guid id);
+        
+        // Admin methods to include soft-deleted entities
+        Task<IEnumerable<T>> GetAllIncludingDeletedAsync();
+        Task<T?> GetByIdIncludingDeletedAsync(Guid id);
+        
     }
 }
